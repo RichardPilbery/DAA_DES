4daa725ad205c5ba6f4aa0894c444ae8a92cfc3c
Sammi Rosser <samanthajaderosser@gmail.com>
2024-11-06 12:12:31 +0000

    Update datetime entry in app

--- app_model.py
+++ app_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import re
+from datetime import datetime
 import streamlit as st
 
 # Temporarily placed in same folder as this as relative imports not behaving with Streamlit
@@ -16,14 +17,18 @@ with st.sidebar:
     st.subheader("Model Inputs")
 
     st.markdown("#### Simulation Run Settings")
-    sim_duration_input =  st.slider("Simulation Duration (days)", 1, 365, 30)
+    sim_duration_input =  st.slider("Simulation Duration (days)", 1, 30, 10)
 
-    warm_up_duration =  st.slider("Warm-up Duration (hours)", 1, 24*10, 72)
+    warm_up_duration =  st.slider("Warm-up Duration (hours)", 0, 24*10, 0)
     st.markdown(f"The simulation will not start recording metrics until {(warm_up_duration / 24):.2f} days have elapsed")
 
     number_of_runs_input = st.slider("Number of Runs", 1, 100, 5)
 
-    sim_start_date_input = st.date_input("Enter the Simulation Start Date")
+    sim_start_date_input = st.date_input("Enter the Simulation Start Date", value=datetime.strptime("2024-08-01 07:00:00", '%Y-%m-%d %H:%M:%S'))
+
+    sim_start_time_input = st.time_input("Enter the Simulations Start Time", value=datetime.strptime("2024-08-01 07:00:00", '%Y-%m-%d %H:%M:%S'))
+
+
 
 button_run_pressed = st.button("Run simulation")
 
@@ -41,7 +46,7 @@ if button_run_pressed:
                     total_runs = number_of_runs_input,
                     sim_duration =sim_duration_input * 24 * 60 ,
                     warm_up_time = warm_up_duration * 60,
-                    sim_start_date = sim_start_date_input
+                    sim_start_date = datetime.combine(sim_start_date_input, sim_start_time_input)
                 )
 
             results.append(
