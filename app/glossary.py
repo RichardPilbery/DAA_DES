93b15cfaf65cc5072e5a33fbbda7084da3df9924
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-28 20:23:07 +0000

    Add placeholder glossary page

---
+++ app/glossary.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+with open("app/style.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+col1, col2 = st.columns([0.7, 0.3])
+
+with col1:
+    st.title("Glossary of Modelling Terms")
+
+with col2:
+    st.image("app/assets/daa-logo.svg", width=300)
+
+st.write("Coming Soon!")
