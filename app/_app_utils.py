1c5a11dd9ca163b0d092d53a3165381911e30164
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-28 19:40:15 +0000

    Add icon metric function and font imports

---
+++ app/_app_utils.py
@@ -0,0 +1,54 @@
+from streamlit_extras.stylable_container import stylable_container
+
+def iconMetricContainer(key,icon_unicode,css_style=None,icon_color='grey', family="filled", type="icons"):
+    """Function that returns a CSS styled container for adding a Material Icon to a Streamlit st.metric value
+
+    CREDIT for starter version of this code: https://discuss.streamlit.io/t/adding-an-icon-to-a-st-metric-easily/59140?u=sammi1
+
+    Args:
+        key (str): Unique key for the component
+        iconUnicode (str): Code point for a Material Icon, you can find them here https://fonts.google.com/icons. Sample \e8b6
+        css_style(str, optional): Additional CSS to apply
+        icon_color (str, optional): HTML Hex color value for the icon. Defaults to 'grey'.
+        family(str, optional): "filled" or "outline". Only works with type = "icons"
+        type(str, optional): "icons" or "symbols"
+
+    Returns:
+        DeltaGenerator: A container object. Elements can be added to this container using either the 'with'
+        notation or by calling methods directly on the returned object.
+    """
+
+    if (family == "filled") and (type=="icons"):
+        font_family = "Material Icons"
+    elif (family == "outline") and (type=="icons"):
+        font_family = "Material Icons Outlined"
+    # elif (family == "filled") and (type=="symbols"):
+    #     font_family = "Material Symbols"
+    elif type=="symbols":
+        font_family = "Material Symbols Outlined"
+    else:
+        print("ERROR - Check Params for iconMetricContainer")
+        font_family = "Material Icons"
+
+    css_style_icon=f'''
+                    div[data-testid="stMetricValue"]>div::before
+                    {{
+                        font-family: {font_family};
+                        content: "\{icon_unicode}";
+                        vertical-align: -20%;
+                        color: {icon_color};
+                    }}
+                    '''
+
+    if css_style is not None:
+        css_style_icon += """
+
+        """
+
+        css_style_icon += css_style
+
+    iconMetric=stylable_container(
+                key=key,
+                css_styles=css_style_icon
+            )
+    return iconMetric
