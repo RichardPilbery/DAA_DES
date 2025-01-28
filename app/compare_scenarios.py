1160617c07135582864b49fb671cb48d2730833a
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-28 20:44:33 +0000

    Begin adding metrics to scenario comparison demo

--- app/compare_scenarios.py
+++ app/compare_scenarios.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from _app_utils import iconMetricContainer, file_download_confirm
 
 st.set_page_config(layout="wide")
 
@@ -13,14 +14,74 @@ with col1:
 with col2:
     st.image("app/assets/daa-logo.svg", width=300)
 
-col_scenario_1, col_scenario_blank, col_scenario_2 = st.columns([0.45, 0.1, 0.45])
+if not st.session_state.scenario_1_set:
+    st.info("Scenario 1 has not been defined. Using Default (Current Operating Scenario) Parameters.")
+if not st.session_state.scenario_2_set:
+    st.error("Scenario 2 has not been defined. Please define a scenario to enable the run button on this page.")
 
-with col_scenario_1:
-    st.header("Scenario 1")
+    if st.button("Click here to go to the parameter page and define scenario 2",
+                type="primary"):
+        st.switch_page("setup.py")
 
-    st.write("Coming Soon!")
 
-with col_scenario_2:
-    st.header("Scenario 2")
+if st.session_state.scenario_2_set:
+    button_run_pressed = st.button("Run simulations")
 
-    st.write("Coming Soon!")
+    if button_run_pressed:
+        @st.fragment
+        def download_button_quarto():
+            # st.download_button(
+            st.button(
+                "Click here to download these results as a file",
+                on_click=file_download_confirm,
+                icon=":material/download:"
+                )
+
+        download_button_quarto()
+
+        st.info(f"All Metrics are averaged across {st.session_state.number_of_runs_input} simulation runs")
+
+
+        col_scenario_1, col_scenario_blank, col_scenario_2 = st.columns([0.45, 0.1, 0.45])
+
+        with col_scenario_1:
+            st.header("Scenario 1")
+
+            if st.session_state.scenario_1_set:
+                st.info("User defined scenario")
+            else:
+                st.success("Default scenario")
+
+            with iconMetricContainer(key="nonattend_metric", icon_unicode="e0b0"):
+                    st.metric("Number of Calls DAAT Resource Couldn't Attend",
+                            "184 of 1203 (15.3%)",
+                            border=True)
+                    st.write(":red[Fewer calls were attended in this scenario]")
+
+                    st.caption("""
+These are the 'missed' calls where no DAAT resource was available.
+This could be due to
+- no resource being on shift
+- all resources being tasked to other jobs at the time of the call
+""")
+
+
+        with col_scenario_2:
+            st.header("Scenario 2")
+
+            # TODO: Remove hardcoding and add logic
+            st.info("User defined scenario: +1 helicopters")
+
+            with iconMetricContainer(key="nonattend_metric", icon_unicode="e0b0"):
+                    st.metric("Number of Calls DAAT Resource Couldn't Attend",
+                            "47 of 1203 (3.9%)",
+                            delta="-11.4% (137 more calls attended)",
+                            delta_color="inverse",
+                            border=True)
+                    st.write(":green[More calls were attended in this scenario]")
+                    st.caption("""
+These are the 'missed' calls where no DAAT resource was available.
+This could be due to
+- no resource being on shift
+- all resources being tasked to other jobs at the time of the call
+""")
