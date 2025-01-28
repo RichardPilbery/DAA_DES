e6e26271afea804fccfa95c0ea6a51bc647dbd39
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-28 18:50:59 +0000

    Initial layout of scenario comparison page

--- app/compare_scenarios.py
+++ app/compare_scenarios.py
@@ -5,7 +5,22 @@ st.set_page_config(layout="wide")
 with open("app/style.css") as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
+col1, col2 = st.columns([0.7, 0.3])
 
-st.title("Compare Scenarios")
+with col1:
+    st.title("Compare Scenarios")
 
-st.write("Coming Soon!")
+with col2:
+    st.image("app/assets/daa-logo.svg", width=300)
+
+col_scenario_1, col_scenario_blank, col_scenario_2 = st.columns([0.45, 0.1, 0.45])
+
+with col_scenario_1:
+    st.header("Scenario 1")
+
+    st.write("Coming Soon!")
+
+with col_scenario_2:
+    st.header("Scenario 2")
+
+    st.write("Coming Soon!")
