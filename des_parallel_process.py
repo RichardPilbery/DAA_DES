278199d9dcb907a24555b47a106f60533073609f
Richard Pilbery <email@pilbery.co.uk>
2025-01-29 08:59:14 +0000

    Addresses issue of over-estimating calls and managing multiple calls in an hour #54

--- des_parallel_process.py
+++ des_parallel_process.py
@@ -89,7 +89,7 @@ def parallelProcessJoblib(total_runs: int, sim_duration: int, warm_up_time: int,
 
 if __name__ == "__main__":
     removeExistingResults()
-    parallelProcessJoblib(1, (2*365*24*60), (0*60), datetime.strptime("2022-07-24 09:19:00", "%Y-%m-%d %H:%M:%S"), False)
+    parallelProcessJoblib(5, (2*365*24*60), (0*60), datetime.strptime("2022-07-24 09:19:00", "%Y-%m-%d %H:%M:%S"), False)
 
 # Testing ----------
 # python des_parallel_process.py
