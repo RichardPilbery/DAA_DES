1da41a82557e81d17f84260d21429e683f6458ba
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-29 01:42:57 +0000

    Begin addng in placeholders in model info page

--- app/info.py
+++ app/info.py
@@ -13,4 +13,26 @@ with col1:
 with col2:
     st.image("app/assets/daa-logo.svg", width=300)
 
-st.write("Coming Soon!")
+
+tab_demand_data,tab_activity_durations, tab_stand_downs = st.tabs(["Demand Data", "Activity Durations", "Stand-downs"])
+
+with tab_demand_data:
+    st.header("How has the data for demand been calculated?")
+
+    st.write("Coming Soon!")
+
+    st.subheader("Isn't this an underestimate of the true demand?")
+
+    st.write("Coming Soon!")
+
+
+
+with tab_activity_durations:
+    st.header("How are the durations of calls calculated?")
+
+    st.write("Coming Soon!")
+
+with tab_stand_downs:
+    st.header("How are stand-downs calculated?")
+
+    st.write("Coming Soon!")
