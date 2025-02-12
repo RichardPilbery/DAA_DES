cadc92892544d8ec93a07bc0f6576ccc837c7f2d
Sammi Rosser <samanthajaderosser@gmail.com>
2025-02-12 16:08:58 +0000

    Add placeholders for glossary and info text

--- app/glossary.py
+++ app/glossary.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import pandas as pd
 
+from _app_utils import get_text, get_text_sheet
+
 st.set_page_config(layout="wide")
 
 with open("app/style.css") as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
+text_df=get_text_sheet("glossary")
+
 col1, col2 = st.columns([0.7, 0.3])
 
 with col1:
-    st.title("Glossary of Modelling Terms")
+    st.title(get_text("page_title", text_df))
 
 with col2:
     st.image("app/assets/daa-logo.svg", width=300)
+
+st.caption(get_text("page_description", text_df))
 
 st.dataframe(
     pd.read_csv("app/glossary.csv"),
