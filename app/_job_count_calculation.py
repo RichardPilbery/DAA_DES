e5504a683a16f0195a50667a056e59b9e51cd0d2
Sammi Rosser <samanthajaderosser@gmail.com>
2025-02-19 14:11:54 +0000

    Fix range display in jobs per month calcs

--- app/_job_count_calculation.py
+++ app/_job_count_calculation.py
@@ -152,26 +152,48 @@ def plot_monthly_calls(call_df, show_individual_runs=False, use_poppins=False,
     call_df['timestamp_dt'] = pd.to_datetime(call_df['timestamp_dt'])
     call_df['month_start'] = call_df['timestamp_dt'].dt.to_period('M').dt.to_timestamp()
 
-    call_counts_monthly = call_df.groupby(['run_number', 'month_start'])[['P_ID']].count().reset_index()
+    call_counts_monthly = call_df.groupby(
+        ['run_number', 'month_start']
+        )[['P_ID']].count().reset_index().rename(columns={"P_ID": "monthly_calls"})
 
     # Identify first and last month in the dataset
     first_month = call_counts_monthly["month_start"].min()
     last_month = call_counts_monthly["month_start"].max()
 
     # Filter out the first and last month
-    call_counts_monthly = call_counts_monthly[(call_counts_monthly["month_start"] != first_month) & (call_counts_monthly["month_start"] != last_month)]
+    call_counts_monthly = (
+        call_counts_monthly[
+            (call_counts_monthly["month_start"] != first_month) &
+            (call_counts_monthly["month_start"] != last_month)
+            ]
+    )
 
-    # Compute statistics
-    summary = call_counts_monthly.groupby("month_start")["P_ID"].agg(["mean", "std", "count"]).reset_index()
-    summary["ci95_hi"] = summary["mean"] + 1.96 * (summary["std"] / np.sqrt(summary["count"]))
-    summary["ci95_lo"] = summary["mean"] - 1.96 * (summary["std"] / np.sqrt(summary["count"]))
+    # Compute mean of number of patients, standard deviation, and total number of monthly calls
+    # 90th Percentile
+    def q90(x):
+        return x.quantile(0.9)
+
+    # 90th Percentile
+    def q10(x):
+        return x.quantile(0.1)
+
+    summary = (
+            call_counts_monthly.groupby("month_start")["monthly_calls"]
+            .agg(["mean", "std", "count", "max", "min", q10, q90])
+            .reset_index()
+            )
+
+    # summary["ci95_hi"] = summary["mean"] + 1.96 * (summary["std"] / np.sqrt(summary["count"]))
+    # summary["ci95_lo"] = summary["mean"] - 1.96 * (summary["std"] / np.sqrt(summary["count"]))
 
     # Create the plot
     fig = px.line(summary, x="month_start", y="mean",
                 markers=True,
                 labels={"mean": "Average Calls Per Month",
                         "month_start": "Month"},
-                title="Number of Monthly Calls Received in Simulation")
+                title="Number of Monthly Calls Received in Simulation",
+                color_discrete_sequence=[DAA_COLORSCHEME["navy"]]
+                )
 
     if show_individual_runs:
         # Get and reverse the list of runs as plotting in reverse will give a more logical
@@ -184,29 +206,46 @@ def plot_monthly_calls(call_df, show_individual_runs=False, use_poppins=False,
             run_data = call_counts_monthly[call_counts_monthly["run_number"] == run]
             fig.add_trace(
                 go.Scatter(
-                    x=run_data["month_start"], y=run_data["P_ID"],
+                    x=run_data["month_start"], y=run_data["monthly_calls"],
                     mode="lines", line=dict(color="gray", width=2, dash='dot'),
                     opacity=0.6, name=f"Simulation Run {run}", showlegend=True,
                 )
             )
 
-    # Add confidence interval as a shaded region
+    # Add full range as a shaded region
     fig.add_traces([
         go.Scatter(
-            x=summary["month_start"], y=summary["ci95_hi"], mode="lines", line=dict(width=0),
+            x=summary["month_start"], y=summary["max"], mode="lines", line=dict(width=0),
             showlegend=False
         ),
         go.Scatter(
-            x=summary["month_start"], y=summary["ci95_lo"], mode="lines", fill="tonexty",
+            x=summary["month_start"], y=summary["min"], mode="lines", fill="tonexty",
+            line=dict(width=0), fillcolor="rgba(0, 176, 185, 0.15)",
+            # fillcolor=DAA_COLORSCHEME['verylightblue'],
+            # opacity=0.1,
+            showlegend=True, name="Full Range Across Simulation Runs"
+        )
+    ])
+
+    # Add 10th-90th percentile interval as a shaded region
+    fig.add_traces([
+        go.Scatter(
+            x=summary["month_start"], y=summary["q90"], mode="lines", line=dict(width=0),
+            showlegend=False
+        ),
+        go.Scatter(
+            x=summary["month_start"], y=summary["q10"], mode="lines", fill="tonexty",
             line=dict(width=0), fillcolor="rgba(0, 176, 185, 0.3)",
             # fillcolor=DAA_COLORSCHEME['verylightblue'],
             # opacity=0.1,
-            showlegend=True, name="95% Range Across Simulation Runs"
+            showlegend=True, name="80% Range Across Simulation Runs"
         )
     ])
 
 
-    fig = fig.update_yaxes({'range': (0, call_counts_monthly["P_ID"].max()*1.1)})
+
+
+    fig = fig.update_yaxes({'range': (0, call_counts_monthly["monthly_calls"].max()*1.1)})
 
     if show_historical:
         historical_jobs_per_month = pd.read_csv(historical_monthly_job_data_path, parse_dates=False)
@@ -215,7 +254,7 @@ def plot_monthly_calls(call_df, show_individual_runs=False, use_poppins=False,
         historical_jobs_per_month["Year_Numeric"] = historical_jobs_per_month["Month"].apply(lambda x: x.year)
         historical_jobs_per_month["New_Date"] = historical_jobs_per_month["Month"].apply(lambda x: datetime.date(year=first_month.year,day=1,month=x.month))
 
-        if (historical_jobs_per_month["Jobs"].max() * 1.1) > (call_counts_monthly["P_ID"].max()*1.1):
+        if (historical_jobs_per_month["Jobs"].max() * 1.1) > (call_counts_monthly["monthly_calls"].max()*1.1):
             fig = fig.update_yaxes({'range': (0, historical_jobs_per_month["Jobs"].max() * 1.1)})
 
         if show_historical_individual_years:
@@ -230,8 +269,9 @@ def plot_monthly_calls(call_df, show_individual_runs=False, use_poppins=False,
                     mode='lines',
                     name=str(year),  # Using the year as the trace name
                     line=dict(
-                        color=list(DAA_COLORSCHEME.values())[idx]
-                              )  # Default to gray if no specific color found
+                        color=list(DAA_COLORSCHEME.values())[idx],
+                        dash="dash"
+                        )  # Default to gray if no specific color found
                 ))
         else:
             # Now, add the filled range showing the entire historical range
@@ -254,11 +294,10 @@ def plot_monthly_calls(call_df, show_individual_runs=False, use_poppins=False,
                 name='Historical Range',
                 line=dict(color='rgba(0,0,0,0)'),  # Invisible line (just the area)
                 fill='tonexty',  # Fill the area between this and the next trace
-                fillcolor='rgba(255, 164, 0, 0.3)',  # Semi-transparent fill color
+                fillcolor='rgba(255, 164, 0, 0.15)',  # Semi-transparent fill color
             ))
 
-
-
+    # Adjust font to match DAA style
     if use_poppins:
         return fig.update_layout(font=dict(family="Poppins", size=18, color="black"))
     else:
