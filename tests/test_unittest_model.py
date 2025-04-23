8a573c7e17d92229623c90c8fddc825eb0436acd
Sammi Rosser <samanthajaderosser@gmail.com>
2025-04-23 09:43:31 +0100

    Write additional reproducibility tests

--- tests/test_unittest_model.py
+++ tests/test_unittest_model.py
@@ -492,6 +492,47 @@ def test_same_seed_gives_consistent_arrival_pattern_runSim():
       gc.collect()
 
 @pytest.mark.reproducibility
+def test_different_seed_gives_different_arrival_pattern_runSim():
+   try:
+      removeExistingResults(remove_run_results_csv=True)
+
+      RUN = 1
+      TOTAL_RUNS = 1
+      SIM_DURATION = 60 * 24 * 7 * 2
+      WARM_UP_TIME = 0
+      SIM_START_DATE = datetime.strptime("2023-01-01 05:00:00", "%Y-%m-%d %H:%M:%S")
+      AMB_DATA = False
+
+      # Should only differ in random seed
+      results_df_1 = runSim(run=RUN,
+                              total_runs=TOTAL_RUNS,
+                           sim_duration=SIM_DURATION,
+                           warm_up_time=WARM_UP_TIME,
+                           sim_start_date=SIM_START_DATE,
+                           amb_data=AMB_DATA,
+                           random_seed=42).reset_index()
+
+
+      results_df_2 = runSim(run=RUN,
+                              total_runs=TOTAL_RUNS,
+                           sim_duration=SIM_DURATION,
+                           warm_up_time=WARM_UP_TIME,
+                           sim_start_date=SIM_START_DATE,
+                           amb_data=AMB_DATA,
+                           random_seed=13).reset_index()
+
+      arrivals_df_1 = results_df_1[results_df_1["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      arrivals_df_2 = results_df_2[results_df_2["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      assert len(arrivals_df_1) > 0, "Arrivals df 1 is empty"
+      assert len(arrivals_df_2) > 0, "Arrivals df 2 is empty"
+
+      assert not arrivals_df_1.equals(arrivals_df_2), "[FAIL - REPRODUCIBILITY - ARRIVALS] Arrivals are the same when different random seed provided and parameters held constant (runSim function)"
+
+   finally:
+      del results_df_1, results_df_2, arrivals_df_1, arrivals_df_2
+      gc.collect()
+
+@pytest.mark.reproducibility
 def test_same_seed_gives_consistent_arrival_pattern_parallelProcessJoblib():
    SIM_DURATION = 60 * 24 * 7 * 2
    WARM_UP_TIME = 0
@@ -546,6 +587,99 @@ def test_same_seed_gives_consistent_arrival_pattern_parallelProcessJoblib():
       del results_df_1, results_df_2, arrivals_df_1, arrivals_df_2
       gc.collect()
 
+@pytest.mark.reproducibility
+def test_different_seed_gives_different_arrival_pattern_parallelProcessJoblib():
+   SIM_DURATION = 60 * 24 * 7 * 2
+   WARM_UP_TIME = 0
+   SIM_START_DATE = datetime.strptime("2023-01-01 05:00:00", "%Y-%m-%d %H:%M:%S")
+   AMB_DATA = False
+
+   try:
+      removeExistingResults(remove_run_results_csv=True)
+
+      # Run the simulation with only a warm-up period and no actual simulation time
+      parallelProcessJoblib(
+         total_runs=1,
+         sim_duration=SIM_DURATION,
+         warm_up_time=WARM_UP_TIME,
+         sim_start_date=SIM_START_DATE,
+         amb_data=AMB_DATA,
+         master_seed=42
+         )
+
+      collateRunResults()
+
+      results_df_1 = pd.read_csv("data/run_results.csv")
+
+      removeExistingResults(remove_run_results_csv=True)
+
+      # Run the simulation with only a warm-up period and no actual simulation time
+      parallelProcessJoblib(
+         total_runs=1,
+         sim_duration=SIM_DURATION,
+         warm_up_time=WARM_UP_TIME,
+         sim_start_date=SIM_START_DATE,
+         amb_data=AMB_DATA,
+         master_seed=13
+         )
+
+      collateRunResults()
+
+      results_df_2 = pd.read_csv("data/run_results.csv")
+
+      arrivals_df_1 = results_df_1[results_df_1["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      arrivals_df_2 = results_df_2[results_df_2["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      assert len(arrivals_df_1) > 0, "Arrivals df 1 is empty"
+      assert len(arrivals_df_2) > 0, "Arrivals df 2 is empty"
+
+      assert not arrivals_df_1.equals(arrivals_df_2), "[FAIL - REPRODUCIBILITY - ARRIVALS] Arrivals are the same when different random seed provided and parameters held constant (parallelProcessJoblib function)"
+
+   finally:
+      del results_df_1, results_df_2, arrivals_df_1, arrivals_df_2
+      gc.collect()
+
+
+
+@pytest.mark.reproducibility
+def test_different_arrival_pattern_across_runs_parallelProcessJoblib():
+   SIM_DURATION = 60 * 24 * 7 * 2
+   WARM_UP_TIME = 0
+   SIM_START_DATE = datetime.strptime("2023-01-01 05:00:00", "%Y-%m-%d %H:%M:%S")
+   AMB_DATA = False
+
+   try:
+      removeExistingResults(remove_run_results_csv=True)
+
+      # Run the simulation with only a warm-up period and no actual simulation time
+      parallelProcessJoblib(
+         total_runs=2,
+         sim_duration=SIM_DURATION,
+         warm_up_time=WARM_UP_TIME,
+         sim_start_date=SIM_START_DATE,
+         amb_data=AMB_DATA,
+         master_seed=42
+         )
+
+      collateRunResults()
+
+      results_df = pd.read_csv("data/run_results.csv")
+      results_df_1 = results_df[results_df["run_number"] == 1]
+      results_df_2 = results_df[results_df["run_number"] == 2]
+
+      arrivals_df_1 = results_df_1[results_df_1["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      arrivals_df_2 = results_df_2[results_df_2["time_type"]=="arrival"][["P_ID","timestamp_dt"]].reset_index(drop=True)
+      assert len(arrivals_df_1) > 0, "Arrivals df 1 is empty"
+      assert len(arrivals_df_2) > 0, "Arrivals df 2 is empty"
+
+      arrivals_df_1.to_csv("tests/TEST_OUTPUT_test_arrivals_behaviour_parallelProcessJobLib - arrivals_df_1.csv")
+      arrivals_df_2.to_csv("tests/TEST_OUTPUT_test_arrivals_behaviour_parallelProcessJobLib - arrivals_df_2.csv")
+
+
+      assert not arrivals_df_1.equals(arrivals_df_2), "[FAIL - REPRODUCIBILITY - ARRIVALS] Arrivals are the same across different runs across different runs with the parallelProcessJoblib function"
+
+   finally:
+      del results_df_1, results_df_2, arrivals_df_1, arrivals_df_2
+      gc.collect()
 
 
 @pytest.mark.reproducibility
